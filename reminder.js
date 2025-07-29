79634dc217ca34ff4512223153a255cc20a46d61
Hammad Danish <150535174+Hammadfudda@users.noreply.github.com>
2025-07-30 01:36:56 +0500

    last

--- reminder.js
+++ reminder.js
@@ -1,24 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyC-CEJglhiAnovdv67Fam70FSAHvxXUlx8",
-    authDomain: "prayertime-2d693.firebaseapp.com",
-    projectId: "prayertime-2d693",
-    storageBucket: "prayertime-2d693.firebasestorage.app",
-    messagingSenderId: "813829458937",
-    appId: "1:813829458937:web:1a6ab79090bfd8265c0b28",
-    measurementId: "G-K5N4TKV1QF"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-
 
   if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
